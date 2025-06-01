d396c8248d107ac29feafbe6345951c1ed344788
RodrigoAugustoM <202505242582@alunos.estacio.br>
2025-06-01 14:19:35 +0000

    estou tentando evitar erros

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,7 +4,7 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa 
+//
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
@@ -22,6 +22,8 @@ int main() {
     int p2pontosturisticos;
     
     // Cadastro das Cartas:
+    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
+    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     printf("\nRegistre a primeira carta,\n Qual o nome do Pais?\n");
     scanf("%s", p1nome);
     printf("Qual a população do %s?\n", p1nome);
@@ -43,18 +45,11 @@ int main() {
     scanf("%f", &p2pib);
     printf("Quantos pontos turisticos %s possui?\n", p2nome);
     scanf("%d", &p2pontosturisticos);
-
-    
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
+  
     // Exibição dos Dados das Cartas:
-    printf("%s\nPopulação: %d\nArea: %.2f m2\nPIB:  R$ %.2f\nPontos turisticos: %d\n", p1nome, p1populacao, p1area, p1pib, p1pontosturisticos);
-    printf("%s\nPopulação: %d\nArea: %2f m2\nPIB:  R$ %.2f\nPontos turisticos: %d\n", p2nome, p2populacao, p2area, p2pib, p2pontosturisticos);
-
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-    
-
+    printf("%s\nPopulação: %d\nArea: %.2f m²\nPIB:  R$%.2f\nPontos turisticos: %d\n", p1nome, p1populacao, p1area, p1pib, p1pontosturisticos);
+    printf("%s\nPopulação: %d\nArea: %2f m²\nPIB:  R$%.2f\nPontos turisticos: %d\n", p2nome, p2populacao, p2area, p2pib, p2pontosturisticos);
     return 0;
 }
